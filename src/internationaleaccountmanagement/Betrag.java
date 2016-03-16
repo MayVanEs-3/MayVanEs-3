69638d617b08218527d69e68638047c00b580bbc
Vanessa <Vanessa@141.19.155.123>
2016-03-16 16:07:48 -0700

    Initial Commit

---
+++ src/internationaleaccountmanagement/Betrag.java
@@ -0,0 +1,5 @@
+package internationaleaccountmanagement;
+
+class Betrag {
+
+}
---
+++ src/internationaleaccountmanagement/Waehrung.java
@@ -0,0 +1,34 @@
+package internationaleaccountmanagement;
+
+class Waehrung {
+	String Name;
+	String Kürzel;
+	long kurs;
+	
+	public Waehrung(String Name, String kürzel, long kurs){
+		this.Name= Name;
+		this.Kürzel = kürzel;
+		this.kurs = kurs;
+		
+	}
+	
+	public void getKürzel(){
+		
+	}
+	
+	public void getKurs(){
+		
+	}
+	
+	public void getName(){
+		
+	}
+	
+	public long umrechenen(){
+		return 0;
+		
+	}
+	
+	
+
+}
