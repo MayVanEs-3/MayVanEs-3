5a431912f7c399880eaa79141d61196d2e7f456b
MayVanEs-3 <maydah.ahmed@t-online.de>
2016-05-08 11:19:42 +0200

    Memberadministration

--- Memebradministration/src/personenidentity/MembershipList.java
+++ Memebradministration/src/personenidentity/MembershipList.java
@@ -2,72 +2,129 @@ package personenidentity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Iterator;
 
-public class MembershipList extends HashMap implements Map{	
-	
-	
+public class MembershipList extends HashMap implements Map {
+
+	private static final long serialVersionUID = 1L;
 	Member eins = new Member(null, null, 0, 0);
 	public int key = eins.getMitgliederID();
 	public String value = eins.toString();
-	// String value = eins.getMitgliedsJahre + eins.getVorname+ eins.getNachname;
+	
+	private String mitgliederID;
+	private String stringListe;
 
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
+			private int pos = -1;
+			private Member stringListe;
+
+			/**
+			 * liefert true solange Iterator nicht Ende der Collection erreich
+			 * hat
+			 */
+			public boolean hasNext() {
+				return (this.pos + 1 < this.stringListe.mitgliedsJahre);
+			}
+
+			/**
+			 * ermittelt nächstes Element
+			 */
+			public String next() throws NoSuchElementException {
+
+				if (!hasNext()) {
+					throw new NoSuchElementException("No more elements");
+				}
+				return null;
+			}
+
+			/**
+			 * entfernt aktuelles Element aus Iterator - hier nicht möglich
+			 * gemacht
+			 */
+			public void remove() throws UnsupportedOperationException {
+				{
+					throw new UnsupportedOperationException("Operation is not supported");
+				}
+			}
+		};
+	}
+
+	/**
+	 * gibt die Anzahl der Elemente in der Liste aus
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
+	/**
+	 * gibt true zurück wenn key/value nicht vorhanden
+	 * 
+	 * @return
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if (size() == 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 
-
+	/**
+	 * true wenn Map Zuordnung für den spezifischen Schlüssel enthält
+	 */
 	@Override
 	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return false;
+		if (!stringListe.contains((CharSequence) key)) {
+			return false;
+		} else
+			return true;
 	}
 
+	/**
+	 * gibt true aus wenn key mindestens auf ein value zugreifen kann
+	 */
+	public boolean containsValue(Member value) {
+		if (!stringListe.contains((CharSequence) value)) {
+			return false;
+		} else
+			return true;
 
-	@Override
-	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
-
+	/**
+	 * gibt Objekt zurück
+	 */
 	@Override
 	public Object get(Object key) {
 		// TODO Auto-generated method stub
-		return null;
+		return key;
 	}
 
-
-	@Override
-	public Object put(Object key, Object value) {
+	/**
+	 * befüllt die Liste mit key/value
+	 */
+	public Object put(Object key, Member value) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
+	/**
+	 * entfernt Objekt von der Liste
+	 */
 	@Override
 	public Object remove(Object key) {
-		// TODO Auto-generated method stub
-		return null;
+		{
+			throw new UnsupportedOperationException();
+		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	public String toString() {
+		return String.format(null, key, value);
+	}
 }
